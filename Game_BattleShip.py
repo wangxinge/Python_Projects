b11d60f8852324ef9f39b05050a55d1ef03612d9
singer1989 <xingewangfdu@gmail.com>
2016-07-26 14:16:15 -0500

    BattleShip

---
+++ Game_BattleShip.py
@@ -0,0 +1,44 @@
+# Battleship Game
+
+from random import randint
+# Plotting Board
+board = []
+
+for x in range(0, 5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+# Assign the battle ship from random row number and col number
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+# Input guess from terminal
+guess_row = int(raw_input("Guess Row:"))
+guess_col = int(raw_input("Guess Col:"))
+
+
+# Update your board
+if guess_row==ship_row and guess_col==ship_col:
+    print "Congratulations! You sank my battleship!"
+elif guess_row in range(5) and guess_col in range(5):
+    if board[guess_row][guess_col]=='X':
+        print "You guessed that one already."
+else:
+    if guess_row not in range(5) or guess_col not in range(5):
+        print "Oops, that's not even in the ocean."
+    else:
+        board[guess_row][guess_col]='X'
+        print_board(board)
+        print "You missed my battleship!"
+
